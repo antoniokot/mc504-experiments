796302d74fd4847bb9f1ae404a118c38b2cecd3b
Antonio Hideto Borges Kotsubo <antoniokotsubo@gmail.com>
2024-08-20 14:15:35 -0300

    update exp01

--- exp01/simpleshell.c
+++ exp01/simpleshell.c
@@ -2,26 +2,45 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
-#include <sys/wait.h> 
+#include <sys/wait.h>
 
 #define STDOUT 1
 #define FAIL_CODE -1
 #define FALSE 0
 
 int main(int argc, char **argv) {
-  char *path = argv[1];
-  char *command[] = { "whoami", "--version", NULL };
   extern char **environ;
+  char command[100];
 
-  pid_t pid = fork();
-  if (pid == 0) {
-    if (execve(path, command, environ) == FAIL_CODE) {
-      printf("O comando não pode ser executado.");
-    } 
-  } else {
-    waitpid(pid, 0, 0);
+  printf("simple-shell$: ");
+  scanf("%s", command);
+
+  char *args[] = { command, NULL };
+  char *path = strtok(argv[1], ":");
+
+  while (path != NULL) {
+    
+    strcat(path, "/");
+    strcat(path, command);
+
+    printf("path: %s\n", path);
+
+    pid_t pid = fork();
+    if (pid == 0) {
+      if (execve(path, args, environ) == FAIL_CODE) {
+        printf("O comando não pode ser executado: ");
+        perror("execve");
+      } 
+    } else {
+      waitpid(pid, 0, 0);
+    }
+    
+    path = strtok(NULL, ":");
   }
+
+
 
   return 0;
 }
