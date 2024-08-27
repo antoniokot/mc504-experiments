05faf12a283c7d6547acf2b7cfe41d1c3847d21b
Antonio Hideto Borges Kotsubo <antoniokotsubo@gmail.com>
2024-08-26 21:08:22 -0300

    update exp01

--- exp01/simpleshell.c
+++ exp01/simpleshell.c
@@ -15,6 +15,7 @@
 int main(int argc, char **argv) {
   char command[100];
 
+  // get the command from the terminal
   printf("simple-shell$: ");
   scanf(" %[^\n]", command);
 
@@ -22,42 +23,42 @@ int main(int argc, char **argv) {
   strcpy(command_copy, command);
 
   int args_len = 0;
-  char *command_token = strtok(command_copy, " ");
+  char *command_token = strtok(command_copy, " ");          // split the command string by empty spaces
   while (command_token != NULL) {
-    args_len++;
+    args_len++;                                             // get the number of arguments
     command_token = strtok(NULL, " ");
   }
 
-  char *args[args_len + 1];
+  char *args[args_len + 1];                                 // create an array of arguments for the execv 
 
   int i = 0;
-  command_token = strtok(command, " ");
-  while (command_token != NULL) {
-    args[i] = command_token;
+  command_token = strtok(command, " ");                     // split the command string by empty spaces again
+  while (command_token != NULL) {       
+    args[i] = command_token;                                // get all of the arguments
     i++;
     command_token = strtok(NULL, " ");
   }
 
   args[i] = NULL;
 
-  char *path_token = strtok(argv[1], ":");
+  char *path_token = strtok(argv[1], ":");                  // separate the possible multiple pahts
   int success = FALSE;
 
   while (path_token != NULL) {
-    char path[strlen(path_token) + strlen(args[0]) + 2];
-    strcpy(path, path_token);
+    char path[strlen(path_token) + strlen(args[0]) + 2];    // create the path string
+    strcpy(path, path_token);                               // build the path
     strcat(path, "/");
     strcat(path, args[0]);
 
     pid_t pid = fork();
-    if (pid == 0) {
-      execv(path, args);
+    if (pid == 0) {                                         // child process
+      execv(path, args);                                    // execute execv
       exit(FAIL_CODE);
-    } else {
+    } else {                                                // not the child process
       int status;
       wait(&status);
 
-      if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+      if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {  // status of success
         success = TRUE;
         break;
       }
@@ -66,7 +67,7 @@ int main(int argc, char **argv) {
     path_token = strtok(NULL, ":");
   }
 
-  if (!success) {
+  if (!success) {                                           // if the command has failed
     printf("Program does not exist.\n");
     exit(1);
   }
