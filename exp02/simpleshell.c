01e2c501aef5afdf5e64fffe6f4453efa40b3ce7
Antonio Hideto Borges Kotsubo <antoniokotsubo@gmail.com>
2024-09-01 17:51:37 -0300

    update exp02

--- exp02/simpleshell.c
+++ exp02/simpleshell.c
@@ -1,4 +1,7 @@
-// Desenvolvido por: Antônio Hideto Borges Kotsubo (236041) - MC504A_2024S2
+/*
+  Sistemas Operacionais - MC504A_2024S2
+  Desenvolvido por: Antônio Hideto Borges Kotsubo (236041)
+*/ 
 
 #include <unistd.h>
 #include <stdio.h>
@@ -6,6 +9,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #define STDOUT 1
 #define FAIL_CODE -1
@@ -17,21 +21,21 @@ int getNumberOfArguments(char *command) {
   char command_copy[MAX];
   strcpy(command_copy, command);
   int args_len = 0;
-  
-  char *command_token = strtok(command_copy, " ");          // split the command string by empty spaces
+
+  char *command_token = strtok(command_copy, " ");             // split the command string by empty spaces
   while (command_token != NULL) {
-    args_len++;                                             // get the number of arguments
+    args_len++;                                                // get the number of arguments
     command_token = strtok(NULL, " ");
   }
 
   return args_len;
 }
 
-void getArguments(char **args, char *command) {
+void buildArgumentsArray(char **args, char *command) {
   int i = 0;
-  char *command_token = strtok(command, " ");               // split the command string by empty spaces again
+  char *command_token = strtok(command, " ");                 // split the command string by empty spaces again
   while (command_token != NULL) {       
-    args[i] = command_token;                                // get all of the arguments
+    args[i] = command_token;                                  // get all of the arguments
     i++;
     command_token = strtok(NULL, " ");
   }
@@ -39,64 +43,79 @@ void getArguments(char **args, char *command) {
   args[i] = NULL;
 }
 
+int shouldRunBackground(char *command) {
+  int boolean = 0;
+  size_t len = strlen(command);
+  if(command[len - 1] == '&') {                               // checks to see if the command should run in background
+    boolean = 1;
+    command[len - 1] = '\0';
+  }
+
+  return boolean;
+}
+
 int main(int argc, char **argv) {
   char command[MAX];
   
   while(TRUE) {
-    printf("simple-shell$: ");                                // get the command from the terminal
-    // get the command from the terminal
-    if (fgets(command, sizeof(command), stdin) == NULL) {
+    printf("simple-shell$: ");                             
+    if (fgets(command, sizeof(command), stdin) == NULL) {     // get the command from the terminal
       continue;                                               // if fgets fails, continue to the next iteration
     }
 
     size_t len = strlen(command);
-    if (len > 0 && command[len - 1] == '\n') {
+    if (len > 0 && command[len - 1] == '\n') {                // remove the new line character from the command
       command[len - 1] = '\0';
     }
 
-    if (strcmp(command, "") == 0) {
+    if (strcmp(command, "") == 0) {                           // checks to see if the command is empty
       continue;
     }
 
-    if (strcmp(command, "exit") == 0) {
-      break;
+    if (strcmp(command, "exit") == 0) {                       // checks to see if the command is "exit"
+      return 0;                                               // if it is, then terminates the shell
     }
 
-    int args_len = getNumberOfArguments(command);
+    int background = shouldRunBackground(command);
+
+    int args_len = getNumberOfArguments(command);             // get the number of arguments
 
     char *args[args_len + 1];
-    getArguments(args, command);
+    buildArgumentsArray(args, command);                       // then build the arguments array
 
     char paths_copy[MAX];
-    strcpy(paths_copy, argv[1]);
-    char *path_token = strtok(paths_copy, ":");               // separate the possible multiple pahts
-    int success = FALSE;
+    strcpy(paths_copy, argv[1]);                              // copies the paths passed when the program was started
+    char *path_token = strtok(paths_copy, ":");               // separate the possible multiple paths
 
-    while (path_token != NULL) {
-      char path[strlen(path_token) + strlen(args[0]) + 2];    // create the path string
-      strcpy(path, path_token);                               // build the path
-      strcat(path, "/");
-      strcat(path, args[0]);
+    pid_t pid = fork();                                       // forks the process
 
-      pid_t pid = fork();
-      if (pid == 0) {                                         // child process
-        execv(path, args);                                    // execute execv
-        exit(FAIL_CODE);
-      } else {                                                // not the child process
-        int status;
-        wait(&status);
-
-        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {  // status of success
-          success = TRUE;
-          break;
+    if(pid == 0)  {
+      if (background) {                                       // checks to see if the command should run in background
+        int new_terminal = open("/dev/null", O_RDWR);         // we will redirect the son's output to another terminal
+        if (new_terminal == -1) {
+          perror("Não foi possível abrir outro terminal");
+          exit(FAIL_CODE);
         }
+        
+        dup2(new_terminal, STDOUT_FILENO);                    // redirect stdout and stdin to the new terminal  
+        dup2(new_terminal, STDIN_FILENO);
+        close(new_terminal); 
       }
 
-      path_token = strtok(NULL, ":");
-    }
+      while (path_token != NULL) {
+        char path[strlen(path_token) + strlen(args[0]) + 2];  // create the path string
+        strcpy(path, path_token);                             // build the path
+        strcat(path, "/");
+        strcat(path, args[0]);
 
-    if (!success) {                                           // if the command has failed
-      printf("Comando não encontrado.\n");
+        execv(path, args);                                    // executes the command passed by the user
+        path_token = strtok(NULL, ":");
+      }
+
+      printf("Comando não encontrado.\n");                    // if the son process wasn't ended, then something went wrong
+      exit(FAIL_CODE);
+    } else if(!background) {
+      waitpid(pid, NULL, 0);                                  // if the command shouldn't run in background, then the father process should wait
     }
   }
 
