2cbed5bd7f89e0fa47261c17029f06ade4a60800
Antonio Kotsubo <antoniokotsubo@gmail.com>
2024-10-16 22:40:19 -0300

    some update

--- exp04/xv6-riscv/user/run_experiment.c
+++ exp04/xv6-riscv/user/run_experiment.c
@@ -6,22 +6,22 @@
 #include "kernel/types.h"
 #include "kernel/fcntl.h"
 #include "user/user.h"
-#include "stdlib.h"
+// #include "stdlib.h"
 #include "graph.h"
 
 int main (void) {
 
-  int n_cpu_exp = rand() % 8 + 6;      // random int from 6 to 14
-  int n_io_exp = 20 - n_cpu_exp;
+  // int n_cpu_exp = rand() % 8 + 6;      // random int from 6 to 14
+  // int n_io_exp = 20 - n_cpu_exp;
 
-  printf("Rounds of CPU-Bound experiments: %d\n", n_cpu_exp);
-  printf("Rounds of IO-Bound experiments: %d\n", n_io_exp);
+  // printf("Rounds of CPU-Bound experiments: %d\n", n_cpu_exp);
+  // printf("Rounds of IO-Bound experiments: %d\n", n_io_exp);
 
   int sum = 0;
 
-  for (int i = n_cpu_exp; i > 0; i--) {
-    sum += shortestPath();
-  }
+  // for (int i = 10; i > 0; i--) {
+  //   sum += shortestPath();
+  // }
 
   printf("Result: %d\n", sum);
 
