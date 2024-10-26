dae9b46609be4e1aecd1191b5e6e32fc353be28a
Pedro Sanchez Bitencourt <pedrosanbit@gmail.com>
2024-10-26 00:01:03 -0300

    add fork to io bound experiment

--- exp04/xv6-riscv/user/io_bound.c
+++ exp04/xv6-riscv/user/io_bound.c
@@ -6,6 +6,16 @@ void run_io_bound_experiment(int rounds) {
     printf("Running IO-Bound experiment with %d rounds...\n\n", rounds);
     for(int i = 0; i < rounds; i++) {
         printf("Running round %d...\n", i + 1);
-        random_write();
+        int pid = fork();
+        if (pid < 0) {
+            printf("Fork failed");
+            exit(1);
+        } else if (pid == 0) {
+            random_write();
+            exit(0);
+        } else {
+            int retPid = wait(&pid);
+            kill(retPid);
+        }
     }
 }
